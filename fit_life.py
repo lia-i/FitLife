22e9902b1d542ed8fdb23693f0dfd8288de0ef1b
Nataliia Ilyina <nailyina@gmail.com>
2026-09-07 13:43:48 +0300

    исправлены ошибки 26-09-07

--- fit_life.py
+++ fit_life.py
@@ -1,14 +1,12 @@
-# Проект FitLife - MVP версия 1.0
-
-
-class LogicalError(Exception):
-    """Обрабатывает логические ошибки"""
-
-    pass
-
 
 WATER_PER_KG = 30
 ML_FOR_L = 1000
+
+
+class LogicalError(Exception):
+    """Обрабатывает логические ошибки."""
+
+    pass
 
 
 def calculate_bmi(weight, height):
@@ -21,75 +19,57 @@ def calculate_water(weight):
     return round(weight * WATER_PER_KG / ML_FOR_L, 1)
 
 
-# 1. Знакомство
-# Спроси у пользователя имя и сохрани в переменную user_name
+def receive_data(name, type, description, err_text):
+    """Обработка ошибок ввода параметров пользователя."""
+    while True:
+        value = input(f"Введите свой {name} {description}: ")
+        try:
+            if type(value) <= 0:
+                raise LogicalError("{name} должен быть больше 0")
+            else:
+                return type(value)
+        except ValueError:
+            print(err_text)
+        except LogicalError as e:
+            print(f"Ошибка: {e}! Попробуйте ещё раз.")
+
+
+# запрашиваем имя
 user_name = input("Введите свое имя: ")
 
-# Спроси возраст и сохрани в переменную user_age
+# запрашиваем возраст, вес и рост
+user_age = receive_data(
+    'возраст',
+    int,
+    'в годах',
+    'Ошибка: нужно ввести целое число. Попробуйте ещё раз.'
+)
 
-while True:
-    try:
-        user_age = int(input("Введите свой возраст в годах: "))
+user_weight = receive_data(
+    'вес',
+    float,
+    'в киллограмах, например, 108.5',
+    'Ошибка: нужно ввести дробное число. '
+    'Используйте точку как разделитель. Попробуйте ещё раз.'
+)
 
-        if user_age <= 0:
-            raise LogicalError("возраст должен быть больше 0")
-        break
-    except ValueError:
-        print("Ошибка: нужно ввести целое число! Попробуйте ещё раз.")
-    except LogicalError as e:
-        print(f"Ошибка: {e}! Попробуйте ещё раз.")
+user_height = receive_data(
+    'рост',
+    float,
+    'в метрах, например, 1.77',
+    'Ошибка: нужно ввести дробное число. '
+    'Используйте точку как разделитель. Попробуйте ещё раз.'
+)
 
-
-# 2. Сбор данных
-# Запроси вес (в кг) и сохрани в user_weight (тип float)
-
-while True:
-    try:
-        user_weight = float(input("Введите свой вес в киллограмах,"
-                                  " например, 108.5: "))
-
-        if user_weight <= 0:
-            raise LogicalError("вес должен быть больше 0")
-        break
-    except ValueError:
-        print("Ошибка: нужно ввести дробное число число, "
-              "используя точку как разделитель. Попробуйте ещё раз.")
-    except LogicalError as e:
-        print(f"Ошибка: {e}! Попробуйте ещё раз.")
-
-# Запроси рост (в метрах, например 1.75) и сохрани в user_height (тип float)
-
-while True:
-    try:
-        user_height = float(input("Введите свой рост в метрах,"
-                                  " например, 1.68: "))
-
-        if user_height <= 0:
-            raise LogicalError("рост должен быть больше 0")
-        break
-    except ValueError:
-        print("Ошибка: нужно ввести дробное число число, "
-              "используя точку как разделитель. Попробуйте ещё раз.")
-    except LogicalError as e:
-        print(f"Ошибка: {e}! Попробуйте ещё раз.")
-
-# 3. Логика расчетов (Функции как "черный ящик": используем арифметику)
-# Формула ИМТ: вес разделить на (рост в квадрате)
-# Рассчитай bmi (Индекс массы тела)
+# рассчитываем индекс массы тела и необходимый объем воды
 bmi = calculate_bmi(user_weight, user_height)
-
-# Подсчет воды: вес * 30 мл
-# Рассчитай water_needed
 water_needed = calculate_water(user_weight)
 
-
-# 4. Вывод красивого результата
-# Используй f-строку, чтобы вывести приветствие, например: "Привет, Иван!"
-print(" ")
-print(f"Привет, {user_name}!")
-# Выведи возраст, ИМТ (округленный до 1 знака) и норму воды.
-
-print(f"Возраст: {user_age}.")
-print(f"Твой Индекс Массы Тела: {bmi:.1f}.")
-print(f"Рекомендуемая норма воды: {water_needed} л. в день.")
-print("Расчет окончен. Будьте здоровы!")
+# выводим отчет
+print(
+    f"\nПривет, {user_name}!"
+    "\nВозраст: {user_age}."
+    "\nВаш Индекс Массы Тела: {bmi:.1f}."
+    "\nРекомендуемая норма воды: {water_needed} л. в день."
+    "\nРасчет окончен. Будьте здоровы!"
+)
